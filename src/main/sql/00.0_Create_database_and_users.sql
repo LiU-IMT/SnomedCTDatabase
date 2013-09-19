379c226d2306388cc9da7d0dc388a5abf1c9c1f2
danka74 <daniel.karlsson@liu.se>
2013-09-19 14:49:05 +0200

    Using exec plugin to run SQL script files

--- src/main/sql/00.0_Create_database_and_users.sql
+++ src/main/sql/00.0_Create_database_and_users.sql
@@ -1,3 +1,7 @@
+-- Database: "termbind"
+DROP DATABASE IF EXISTS termbind2;
+CREATE DATABASE termbind2;
+
 -- Role: "termbinduser"
 CREATE ROLE termbinduser LOGIN
   ENCRYPTED PASSWORD 'md5c6c5e55d46f1eab8d5ff30f3f502688e'
