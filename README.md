23e7daa76cacbb8810b7e86e41d92bf613157f13
danka74 <daniel.karlsson@liu.se>
2014-08-01 15:21:21 +0200

    Fixed running of scripts from Maven

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 SnomedCTDatabase
 ================
+
+Scripts for installing the SNOMED CT Expression Repository database.
+
+Requirements:
+A running PostgreSQL server.
+Two propoperties files:
+pwd.properties: includes user (and password) with permission to create databases etc.
+snomedct.properties: includes path to SNOMED CT distribution files
+Examples are in the src/main/resources folder.
--- src/main/sql/01.1_Load_release_files_tables.sql
+++ src/main/sql/01.1_Load_release_files_tables.sql
@@ -1,20 +1,21 @@
-COPY concepts_rf2 FROM
-								E'${snomedCTSourceDirectory}Full/Terminology/sct2_Concept_Full_INT_20130731.txt'
-								WITH
-								DELIMITER AS E'\t' CSV HEADER QUOTE AS '|';
+COPY concepts_rf2 FROM 
+:f1
+WITH DELIMITER AS E'\t' CSV HEADER QUOTE AS '|';
 
-								COPY descriptions_rf2 FROM
-								E'${snomedCTSourceDirectory}Full/Terminology/sct2_Description_Full-en_INT_20130731.txt'
-								WITH DELIMITER AS E'\t' CSV HEADER QUOTE AS '`';
+/*
+COPY descriptions_rf2 FROM
+E'/home/daniel/Documents/SCT/SnomedCT_Release_INT_20140131/RF2Release/Full/Terminology/sct2_Description_Full-en_INT_20140131.txt'
+WITH DELIMITER AS E'\t' CSV HEADER QUOTE AS '`';
 
-								COPY descriptions_rf2 FROM
-								E'${snomedCTSourceDirectory}Full/Terminology/sct2_TextDefinition_Full-en_INT_20130731.txt'
-								WITH DELIMITER AS E'\t' CSV HEADER QUOTE AS '`';
+COPY descriptions_rf2 FROM
+E'/home/daniel/Documents/SCT/SnomedCT_Release_INT_20140131/RF2Release/Full/Terminology/sct2_TextDefinition_Full-en_INT_20140131.txt'
+WITH DELIMITER AS E'\t' CSV HEADER QUOTE AS '`';
 
-								COPY relationships_rf2 FROM
-								E'${snomedCTSourceDirectory}Full/Terminology/sct2_Relationship_Full_INT_20130731.txt'
-								WITH DELIMITER AS E'\t' CSV HEADER QUOTE AS '`';
+COPY relationships_rf2 FROM
+E'/home/daniel/Documents/SCT/SnomedCT_Release_INT_20140131/RF2Release/Full/Terminology/sct2_Relationship_Full_INT_20140131.txt'
+WITH DELIMITER AS E'\t' CSV HEADER QUOTE AS '`';
 
-								COPY languagerefsets_rf2 FROM
-								E'${snomedCTSourceDirectory}Full/Refset/Language/der2_cRefset_LanguageFull-en_INT_20130731.txt'
-								WITH DELIMITER AS E'\t' CSV HEADER QUOTE AS '`';
+COPY languagerefsets_rf2 FROM
+E'/home/daniel/Documents/SCT/SnomedCT_Release_INT_20140131/RF2Release/Full/Refset/Language/der2_cRefset_LanguageFull-en_INT_20140131.txt'
+WITH DELIMITER AS E'\t' CSV HEADER QUOTE AS '`';
+*/
